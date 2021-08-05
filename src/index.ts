eeac415dc910a74874a5a53581d137b6ff04233c
Toliak <toliakpurple@gmail.com>
2021-08-05 22:54:40 +0300

    Changed tsconfig target to ES6

--- src/index.ts
+++ src/index.ts
@@ -5,47 +5,52 @@ import { exportToGlobal } from './global_export';
 import removeExportModifierIfPossible from './remove_export_modifier';
 
 export default function (_program: ts.Program, _pluginOptions: {}) {
-  return (ctx: ts.TransformationContext) => {
-    return (sourceFile: ts.SourceFile) => {
-      const props: {
-        side: ImportTypesSide | undefined;
-      } = {
-        side: undefined,
-      };
+    return (ctx: ts.TransformationContext) => {
+        return (sourceFile: ts.SourceFile) => {
+            const props: {
+                side: ImportTypesSide | undefined;
+            } = {
+                side: undefined,
+            };
 
-      const visitor = (node: ts.Node): ts.Node | ts.Node[] | undefined => {
-        // Attempt to detect script side (and to restrict sides mixing)
-        fileSideDetection(node, props);
+            const visitor = (
+                node: ts.Node,
+            ): ts.Node | ts.Node[] | undefined => {
+                // Attempt to detect script side (and to restrict sides mixing)
+                fileSideDetection(node, props);
 
-        // Remove imports => remove lua 'requires'
-        if (isImportNode(node)) {
-          return undefined;
-        }
+                // Remove imports => remove lua 'requires'
+                if (isImportNode(node)) {
+                    return undefined;
+                }
 
-        // Exported functions/classes/variables
-        // will be explicitly set as a property of _G
-        let exportReplace = exportToGlobal(node, ctx);
-        if (exportReplace) {
-          if (typeof exportReplace == 'object') {
-            exportReplace = exportReplace as ts.Node[];
-            return [node, ...exportReplace];
-          } else {
-            exportReplace = exportReplace as ts.Node;
-            return [node, exportReplace];
-          }
-        }
+                // Exported functions/classes/variables
+                // will be explicitly set as a property of _G
+                let exportReplace = exportToGlobal(node, ctx);
+                if (exportReplace) {
+                    if (typeof exportReplace == 'object') {
+                        exportReplace = exportReplace as ts.Node[];
+                        return [node, ...exportReplace];
+                    } else {
+                        exportReplace = exportReplace as ts.Node;
+                        return [node, exportReplace];
+                    }
+                }
 
-        // Attempt to remove `export` modifier and add `_G` prop
-        // Used for inlined `export`, e.g. `export function ...`
-        const removedModifier = removeExportModifierIfPossible(node, ctx);
-        if (removedModifier) {
-          return removedModifier;
-        }
+                // Attempt to remove `export` modifier and add `_G` prop
+                // Used for inlined `export`, e.g. `export function ...`
+                const removedModifier = removeExportModifierIfPossible(
+                    node,
+                    ctx,
+                );
+                if (removedModifier) {
+                    return removedModifier;
+                }
 
-        return ts.visitEachChild(node, visitor, ctx);
-      };
+                return ts.visitEachChild(node, visitor, ctx);
+            };
 
-      return ts.visitEachChild(sourceFile, visitor, ctx);
+            return ts.visitEachChild(sourceFile, visitor, ctx);
+        };
     };
-  };
 }
--- src/remove_export_modifier.ts
+++ src/remove_export_modifier.ts
@@ -7,74 +7,88 @@ type ResultType = ts.Node | ts.Node[] | undefined;
  *  `export function something() {}` will be converted to
  *  `local function something() end; _G.something = something`
  */
-function removeFunctionExportModifier(node: ts.Node, ctx: ts.TransformationContext): ResultType {
-  if (!ts.isFunctionDeclaration(node)) {
-    return undefined;
-  }
-  if (!(ts.getCombinedModifierFlags(node) & ts.ModifierFlags.Export)) {
-    return undefined;
-  }
+function removeFunctionExportModifier(
+    node: ts.Node,
+    ctx: ts.TransformationContext,
+): ResultType {
+    if (!ts.isFunctionDeclaration(node)) {
+        return undefined;
+    }
+    if (!(ts.getCombinedModifierFlags(node) & ts.ModifierFlags.Export)) {
+        return undefined;
+    }
 
-  const name = node.name?.getText() || '___undefined';
+    const name = node.name?.getText() || '___undefined';
 
-  return [node, generateGlobalExpression(name, ctx.factory, true)];
+    return [node, generateGlobalExpression(name, ctx.factory, true)];
 }
 
 /**
  * `export class ETestC { }` will be converted to
  * `local ETestC = __TS__Class(); ...; _G.ETestC = ETestC`
  */
-function removeClassExportModifier(node: ts.Node, ctx: ts.TransformationContext): ResultType {
-  if (!ts.isClassDeclaration(node)) {
-    return undefined;
-  }
-  if (!(ts.getCombinedModifierFlags(node) & ts.ModifierFlags.Export)) {
-    return undefined;
-  }
+function removeClassExportModifier(
+    node: ts.Node,
+    ctx: ts.TransformationContext,
+): ResultType {
+    if (!ts.isClassDeclaration(node)) {
+        return undefined;
+    }
+    if (!(ts.getCombinedModifierFlags(node) & ts.ModifierFlags.Export)) {
+        return undefined;
+    }
 
-  const name = node.name?.getText() || '___undefined';
+    const name = node.name?.getText() || '___undefined';
 
-  return [node, generateGlobalExpression(name, ctx.factory, true)];
+    return [node, generateGlobalExpression(name, ctx.factory, true)];
 }
 
 /**
  * `export let ETestLet: number = 54, ETestLet2: number = 13;` will be converted to
  * `local ETestLet = 54; local ETestLet2 = 13; _G.ETestLet = ETestLet; _G.ETestLet2 = ETestLet2`
  */
-function removeVariableExportModifier(node: ts.Node, ctx: ts.TransformationContext): ResultType {
-  if (!ts.isVariableStatement(node)) {
-    return undefined;
-  }
-  if (!(ts.getCombinedModifierFlags(node as any) & ts.ModifierFlags.Export)) {
-    return undefined;
-  }
+function removeVariableExportModifier(
+    node: ts.Node,
+    ctx: ts.TransformationContext,
+): ResultType {
+    if (!ts.isVariableStatement(node)) {
+        return undefined;
+    }
+    if (!(ts.getCombinedModifierFlags(node as any) & ts.ModifierFlags.Export)) {
+        return undefined;
+    }
 
-  const names: string[] = [];
-  for (const declaration of node.declarationList.declarations) {
-    const name = declaration.name?.getText() || '___undefined';
+    const names: string[] = [];
+    for (const declaration of node.declarationList.declarations) {
+        const name = declaration.name?.getText() || '___undefined';
 
-    names.push(name);
-  }
+        names.push(name);
+    }
 
-  const globalExpressions: ts.Node[] = [];
-  for (const name of names) {
-    globalExpressions.push(generateGlobalExpression(name, ctx.factory, true));
-  }
-  return [node, ...globalExpressions];
+    const globalExpressions: ts.Node[] = [];
+    for (const name of names) {
+        globalExpressions.push(
+            generateGlobalExpression(name, ctx.factory, true),
+        );
+    }
+    return [node, ...globalExpressions];
 }
 
-export default function removeExportModifierIfPossible(node: ts.Node, ctx: ts.TransformationContext): ResultType {
-  const chain: ((n: ts.Node, c: ts.TransformationContext) => ResultType)[] = [
-    removeFunctionExportModifier,
-    removeClassExportModifier,
-    removeVariableExportModifier,
-  ];
-  for (const f of chain) {
-    const result = f(node, ctx);
-    if (result) {
-      return result;
+export default function removeExportModifierIfPossible(
+    node: ts.Node,
+    ctx: ts.TransformationContext,
+): ResultType {
+    const chain: ((n: ts.Node, c: ts.TransformationContext) => ResultType)[] = [
+        removeFunctionExportModifier,
+        removeClassExportModifier,
+        removeVariableExportModifier,
+    ];
+    for (const f of chain) {
+        const result = f(node, ctx);
+        if (result) {
+            return result;
+        }
     }
-  }
 
-  return undefined;
+    return undefined;
 }
