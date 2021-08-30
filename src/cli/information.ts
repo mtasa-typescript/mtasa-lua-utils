0977411d0d86f34c00a49f86dc1495f50b404f30
Toliak <toliakpurple@gmail.com>
2021-08-31 01:47:17 +0300

    #33 Fixed - Changed typescript version to `4.3.5` - Forceful recreated `package-lock.json`

--- src/cli/information.ts
+++ src/cli/information.ts
@@ -1,4 +1,5 @@
 export function getVersion(): string {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
     const { version } = require('../../package.json');
     return version;
 }
